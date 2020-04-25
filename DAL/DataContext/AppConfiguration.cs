77a7e63dae4d27bb10ce32ef8c230e50eeb519b0
PAMALSAHAN <p.s.jayawickrama@gmail.com>
2020-04-25 23:20:54 +0530

    adding layers

---
+++ DAL/DataContext/AppConfiguration.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DAL.DataContext
+{
+    public class AppConfiguration
+    {
+        //ctor ekak hadanawa
+        public AppConfiguration()
+        {
+            //configuration builder ekak use karanawa configuration eka hadaganna
+            var configBuilder=new ConfigurationBuilder();
+            // path ekata gihin appsetting eka hadaganna eka
+            var path=Path.Combine(Directory.GetCurrentDirectory(),"appsetting.json");
+            //path ekata gihin json file eka add karaganna eka
+            configBuilder.AddJsonFile(path,false);
+            //build karaganna eka
+            var root=configBuilder.Build();
+            //section ekak ganna eka connection string eke
+            var appsetting=root.GetSection("connection");
+            // eka prperty ekata add karaganna widiha
+            sqlConnectionString=appsetting.Value;
+        }
+        public string sqlConnectionString { get; set; }
+    }
+}
